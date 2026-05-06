7a7f02e58811b58fc4a5d23ff9f7e90da6a04fd0
Gaurav Alva <gauravgalva@gmail.com>
2026-05-06 10:45:51 +0530

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://gaurav-g-alva.github.io',
-  base: '/',
+  //base: '/',
   output: 'static',
   integrations: [mdx(), sitemap()],
 });
